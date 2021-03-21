8fbc6d9f0b63e341abc3a410a40b9eea2817a72f
radoslaw.rycyk <radoslaw.rycyk@jeppesen.com>
2021-03-21 14:07:18 +0100

    add swagger, lombok, project structure

--- src/main/java/com/bluestone/holiday/Application.java
+++ src/main/java/com/bluestone/holiday/Application.java
@@ -1,10 +1,12 @@
+package com.bluestone.holiday;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class);
+        SpringApplication.run(Application.class);
     }
 }
