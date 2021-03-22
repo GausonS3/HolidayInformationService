da8eca0c0beeca4a17deb3ac3f44b42fc43e48ac
radoslaw.rycyk <radoslaw.rycyk@jeppesen.com>
2021-03-22 13:50:25 +0100

    add swagger documentation, extend readme

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # HolidayInformationService
-Bluestone interview task
+Service accepts two country codes and a date and return next holiday after the given date that will happen on the same 
+day in both countries in the given or following year. 
 
 # Build jar file
-mvn clean package
+mvn clean install
 
 # Build docker image
 docker build --tag=holiday-information:latest .
 
 # Run docker image
-docker run -e NAGER-API-ADDRESS='https://date.nager.at/Api/v2' -p 8080:8080 holiday-information:latest
+docker run -p 8080:8080 holiday-information:latest
+
+configurable envs:
+
+-e NAGER-API-ADDRESS=
+-e SERVER-PORT=
--- src/main/java/com/bluestone/holiday/controller/HolidayInformationController.java
+++ src/main/java/com/bluestone/holiday/controller/HolidayInformationController.java
@@ -4,8 +4,10 @@ import com.bluestone.holiday.model.CountryDto;
 import com.bluestone.holiday.model.MatchingHolidayDto;
 import com.bluestone.holiday.service.CountryValidationService;
 import com.bluestone.holiday.service.HolidayService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.apachecommons.CommonsLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -13,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -29,6 +30,15 @@ public class HolidayInformationController {
     private final CountryValidationService countryValidationService;
 
     @GetMapping("/matching-holiday")
+    @ApiOperation(
+            value = "Accepts two country codes and a date and return next holiday after the given date that will happen on the same \n" +
+                    "day in both countries in the given or following year. ")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 204, message = "No holiday found in given or following year"),
+            @ApiResponse(code = 400, message = "One of the provided country code is invalid"),
+            @ApiResponse(code = 500, message = "External API Error")
+    })
     public ResponseEntity<MatchingHolidayDto> getNextMatchingHoliday(@RequestParam(value = "firstCountryCode") String firstCountryCode,
                                                                      @RequestParam(value = "secondCountryCode") String secondCountryCode,
                                                                      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -42,6 +52,12 @@ public class HolidayInformationController {
     }
 
     @GetMapping("/country-codes")
+    @ApiOperation(
+            value = "Returns list of available country codes that user can request matching holiday for")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 500, message = "External API Error")
+    })
     public ResponseEntity<List<CountryDto>> listAvailableCountryCodes() {
         log.info("List available country codes request received with following params");
         return new ResponseEntity<>(holidayService.getAvailableCountryCodes(), HttpStatus.OK);
